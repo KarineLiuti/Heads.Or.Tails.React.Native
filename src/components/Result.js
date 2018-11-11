8d7c15ee6fb26b836e1a07dcd3f011a4bbe74519
Karine Liuti <kah.liuti@gmail.com>
2018-11-11 16:51:43 -0200

    Made result scene

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
-import { Image, View } from 'react-native';
+import { Image, View, Text, StyleSheet } from 'react-native';
 
 const heads = require('../imgs/moeda_cara.png');
 const tails = require('../imgs/moeda_coroa.png');
 
 export default class OtherGames extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      result: '' 
+    };
+  }
+
+  componentWillMount() {
+    const getResult = Math.floor(Math.random() * 2);
+
+    let result = getResult === 0 ? 'heads' : 'tails';
+
+    this.setState({ result });
+  }
+
 	render() {
-		return (
-			<View style={{ flex: 1, backgroundColor: '#61BD8C' }}>
-				<Image source={heads} />
-        <Image source={tails} />
-			</View>
-		);
+    if (this.state.result === 'heads') {
+      return (
+        <View style={{ flex: 1, backgroundColor: '#61BD8C', padding: 30 }}>
+          <Image source={heads} />
+          <Text style={styles.resultText}>
+            CARA
+          </Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={{ flex: 1, backgroundColor: '#61BD8C', padding: 30 }}>
+          <Image source={tails} />
+          <Text style={styles.resultText}>
+            COROA
+          </Text>
+        </View>
+      );
+    }
 	}
 }
+
+const styles = StyleSheet.create({
+  resultText: {
+    alignSelf: 'center',
+    marginTop: 30,
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#FFF'
+  }
+});
